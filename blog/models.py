0cbc8489e376788171c1485218f91af51fc04634
Erasyl <erasyl490@gmail.com>
2021-04-04 10:08:35 +0600

    tricky

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ import os, random
 
 
 class OwnCompany(models.Model):
-    name = models.CharField('Название Юр. Лица', max_length=200)
+    name = models.CharField('Название Юр.  Лица', max_length=200)
     ebin = models.CharField('БИН РК', max_length=14, blank=True)
 
 
